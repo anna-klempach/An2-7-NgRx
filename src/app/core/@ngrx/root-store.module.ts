aaf57b693ef31d7227748b7f3060d4dc04f0fc82
Hanna Klempach <hklempach@wideorbit.com>
2023-04-17 23:26:58 +0200

    Task 22. Router State

--- src/app/core/@ngrx/root-store.module.ts
+++ src/app/core/@ngrx/root-store.module.ts
@@ -6,13 +6,14 @@ import { metaReducers } from './meta-reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './../../../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
-
+import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
+import { routerReducers, CustomSerializer } from './router';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot({}, {
+    StoreModule.forRoot(routerReducers, {
       metaReducers,
       // All checks will automatically be disabled in production builds
       runtimeChecks: {
@@ -23,6 +24,11 @@ import { EffectsModule } from '@ngrx/effects';
         strictActionWithinNgZone: true, // default value is false
         strictActionTypeUniqueness: true // default value is false
       }
+    }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+      routerState: RouterState.Minimal,
+      serializer: CustomSerializer // has a priority over routerState
     }),
     EffectsModule.forRoot([]),
     TasksStoreModule,
