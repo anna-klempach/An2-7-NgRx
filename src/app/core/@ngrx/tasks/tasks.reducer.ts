7637a458023769e658f2515d93e0bd450eabcabc
Hanna Klempach <hklempach@wideorbit.com>
2023-04-17 14:35:28 +0200

    Task 04. Create a Reducer

---
+++ src/app/core/@ngrx/tasks/tasks.reducer.ts
@@ -0,0 +1,30 @@
+import { createReducer, on } from '@ngrx/store';
+import { initialTasksState } from './tasks.state';
+import * as TasksActions from './tasks.actions';
+export const reducer = createReducer(
+  initialTasksState,
+  on(TasksActions.getTasks, state => {
+    console.log('GET_TASKS action being handled!');
+    return { ...state };
+  }),
+  on(TasksActions.getTask, state => {
+    console.log('GET_TASK action being handled!');
+    return { ...state };
+  }),
+  on(TasksActions.createTask, state => {
+    console.log('CREATE_TASK action being handled!');
+    return { ...state };
+  }),
+  on(TasksActions.updateTask, state => {
+    console.log('UPDATE_TASK action being handled!');
+    return { ...state };
+  }),
+  on(TasksActions.completeTask, state => {
+    console.log('COMPLETE_TASK action being handled!');
+    return { ...state };
+  }),
+  on(TasksActions.deleteTask, state => {
+    console.log('DELETE_TASK action being handled!');
+    return { ...state };
+  })
+);
